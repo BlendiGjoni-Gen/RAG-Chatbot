8694c95162f25bf0120885382eb9bb8c730b9c66
BlendiGjoniGen <Blendi.Gjoni@Genpact.com>
2026-01-18 20:15:02 +0100

    Hugging Face api hosting

--- rag/llm.py
+++ rag/llm.py
@@ -1,14 +1,12 @@
-from dotenv import load_dotenv
+import os
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
-
-load_dotenv()
-
 
 def get_llm():
     llm = HuggingFaceEndpoint(
         repo_id="meta-llama/Llama-3.1-8B-Instruct",
-        stop_sequences=["Answer:", "\n\n", "DONE", "<|eot_id|>"],
         temperature=0.1,
-        max_new_tokens=512
+        max_new_tokens=512,
+        huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+        stop_sequences=["DONE"]
     )
     return ChatHuggingFace(llm=llm)
